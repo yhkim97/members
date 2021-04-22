1328d4d3c4694315450010d3ef3e25b8c32eebe0
yhkim97 <rladydgml82@naver.com>
2021-04-22 12:32:25 +0900

    리스트

---
+++ setMemberList.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Iterator;
+
+class setMemberList {
+  public static void main(String[] args) {
+        // 입력
+        String[] valueArr =  new String[10];
+        valueArr[0] = "A2020";
+        valueArr[1] = "A2021";
+        valueArr[2] = "A2022";
+        System.out.println(valueArr[0]);
+        // 1-2. Map에 배열 넣기
+        // Map<String, String[]> arrMap = new HashMap<>();
+        // arrMap.put("key01", valueArr[0]);
+        // arrMap.put("key02", valueArr[1]);
+        // arrMap.put("key03", valueArr[2]);
+
+
+        // // 출력
+        // Iterator<String> mapIter = arrMap.keySet().iterator();
+
+        // while (mapIter.hasNext()) {
+
+        //     String key = mapIter.next();
+        //     String value[] = arrMap.get(key);
+        //     // System.out.println(key);
+
+        //     System.out.println(key + " : " + value[0]);
+
+        // }   
+  }
+}
